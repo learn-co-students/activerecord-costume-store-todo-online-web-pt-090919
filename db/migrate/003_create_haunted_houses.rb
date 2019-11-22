6911baf128113975c3c0576325b23fc1f4a6d454
Jared S <jaredjames2020@gmail.com>
2019-11-22 19:27:02 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.string :description
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,25 +12,14 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_store", force: :cascade do |t|
+  create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
     t.integer "num_of_employees"
-    t.string "still_in_business"
+    t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.integer "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-    t.string "description"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -40,6 +29,17 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "size"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.integer "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.string "description"
   end
 
 end
